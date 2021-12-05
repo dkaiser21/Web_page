6e48bae54e435bffdb39efb9d881c732380fcd72
Cahedan <f.guajardo734@gmail.com>
2021-12-05 11:03:47 -0400

    first commit

---
+++ FrontEnd/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<link rel="stylesheet" href="/FrontEnd/formato.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roleando</title>
+</head>
+<body>
+
+</body>
+</html>
